5c146c4de67b8935cca32a3b13acf93e2ee7dc00
Hyeok kiki <65284583+honghyeokgi@users.noreply.github.com>
2021-05-17 11:16:10 +0900

    Update 약수의 합.js

--- 구름/약수의 합.js
+++ 구름/약수의 합.js
@@ -1,1 +1,27 @@
 
+const readline = require("readline");
+var str = "";
+var sum = 0;
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on("line", function(line) {
+		for( var i=2; i <= line ; i++){
+		if(line % i == 0){
+			str += " "+i
+			
+		}
+	}
+str = "1"+str;
+	var a = str.split(" ");
+	
+	for(var j = 0 ; j <= a.length-1; j++){
+		sum += Number(a[j])
+	}
+	console.log(sum);
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
