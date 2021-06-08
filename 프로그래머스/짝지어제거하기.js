115fd4da229fd6171f2850b00b46d9b5eda5cac5
Hyeok kiki <65284583+honghyeokgi@users.noreply.github.com>
2021-06-08 16:00:14 +0900

    Create 짝지어제거하기.js

---
+++ 프로그래머스/짝지어제거하기.js
@@ -0,0 +1,17 @@
+function solution(s)
+{
+    var answer = ''
+    var arr = []
+    for(let i = 0 ; i <s.length; i++){
+        arr.push(s[i])
+        if(arr[arr.length-1] === arr[arr.length-2]){
+            arr.pop();
+            arr.pop();
+        }
+    }
+    
+    
+    return answer = arr.length > 0 ?  answer = 0 : answer = 1;;
+}
+
+stack 형태로 풀이 하였습니다
